49b0a5f1bcafa2c300d9f6c06bf3166c10e70bea
Erick Rodriguez <145071761+eriayay@users.noreply.github.com>
2023-10-05 19:26:27 -0500

    Update Hotel.py

--- Hotel.py
+++ Hotel.py
@@ -8,11 +8,19 @@ class Persona:
 
 #clase Habitacion
 class Habitacion:
-    def __init__(self, numero):
-        self.numero = numero
+    def __init__(self, numeroHab):
+        self.numeroHab = numeroHab
         self.huesped = None
         self.siguiente = None
         
+
+# Clase para guardar la informacion de las entradas
+class EntradaPersona:
+    def __init__(self, persona, numeroHab):
+        self.persona = persona
+        self.numeroHab = numeroHab
+        self.siguiente = None
+
 
 #Clase lista de personas
 class ListaPersonas:
@@ -53,8 +61,8 @@ class ListaHabitaciones:
         self.cabeza = None
         
     #Registrar habitaciones
-    def registrar(self, numero):
-        nueva_habitacion = Habitacion(numero)
+    def registrar(self, numeroHab):
+        nueva_habitacion = Habitacion(numeroHab)
         
         if self.cabeza == None:
             self.cabeza = nueva_habitacion
@@ -65,11 +73,11 @@ class ListaHabitaciones:
                 
             habitacion_actual.siguiente = nueva_habitacion
             
-    #Buscar habitacion por numero
-    def buscar(self, numero):
+    #Buscar habitacion por numeroHab
+    def buscar(self, numeroHab):
         habitacion_actual = self.cabeza
         while(habitacion_actual):
-            if habitacion_actual.numero == numero:
+            if habitacion_actual.numeroHab == numeroHab:
                 break
             habitacion_actual = habitacion_actual.siguiente
 
@@ -79,8 +87,8 @@ class ListaHabitaciones:
             return habitacion_actual
 
     #Asignar habitacion
-    def asignar(self, numero, persona):
-        habitacion_actual = buscar(numero)
+    def asignar(self, numeroHab, persona):
+        habitacion_actual = buscar(numeroHab)
         if habitacion_actual != None:
             habitacion_actual.huesped = persona
         else:
@@ -92,7 +100,7 @@ class ListaHabitaciones:
         habitacion_actual = self.cabeza
         while(habitacion_actual):
             if habitacion_actual.huesped == None:
-                print(habitacion_actual.numero)
+                print(habitacion_actual.numeroHab)
             habitacion_actual = habitacion_actual.siguiente
         
     #Imprimir habitaciones disponibles
@@ -101,14 +109,55 @@ class ListaHabitaciones:
         habitacion_actual = self.cabeza
         while(habitacion_actual):
             if habitacion_actual.huesped != None:
-                print(habitacion_actual.numero)
+                print(habitacion_actual.numeroHab)
             habitacion_actual = habitacion_actual.siguiente
 
 
 #clase lista libro de entradas
-class Entrada:
-    def __init__(self):
+class Entradas:
+    def __init__(self, listaPersonas, listaHabitaciones):
         self.cabeza = None
+        self.listaPersonas = listaPersonas
+        self.listaHabitaciones = listaHabitaciones
 
     #Agregar personas a la lista
-    def agregar(self):
+    def agregar(self, cedula, numeroHab):
+        persona = self.listaPersonas.buscar(cedula)
+        if persona:
+            nueva_entrada = EntradaPersona(persona, numeroHab)
+
+            if self.cabeza is None:
+                self.cabeza = nueva_entrada
+            else:
+                entrada_actual = self.cabeza
+                while (entrada_actual.siguiente):
+                    entrada_actual = entrada_actual.siguiente
+                entrada_actual.siguiente = nueva_entrada
+                # Tambien asigna la habitacion a la persona en la lista de habitaciones
+                self.listaHabitaciones.asignar(numeroHab, persona)
+            print("Asignacion correctamente realizada")
+        else:
+            print("Persona no registrada, por favor registrar primero")
+
+    #Imprimir lista de entradas
+    def imprimirEntradas(self):
+        print("\nLibro de entradas:")
+        entrada_actual = self.cabeza
+        while entrada_actual:
+            print("Cedula: " + str(entrada_actual.persona.cedula) + " Nombre: " + str(entrada_actual.persona.nombre) + " Habitacion: " + str(entrada_actual.numero_habitacion))
+            entrada_actual = entrada_actual.siguiente
+
+    #Imprimir entrada individual
+    def imprimirIndividual(self, cedula):
+        persona = self.listaPersonas.buscar(cedula)
+        entrada_actual = self.cabeza
+                while (entrada_actual):
+                    if entrada_actual.persona == persona:
+                        break
+                    entrada_actual = entrada_actual.siguiente
+
+                if entrada_actual.persona == persona:    
+                    print("\nPersona actualmente en el hotel")
+                    print("Cedula: " + str(entrada_actual.persona.cedula) + " Nombre: " + str(entrada_actual.persona.nombre) + " Habitacion: " + str(entrada_actual.numero_habitacion))
+                else:
+                    print("\nPersona no reside actualmente en el hotel")       
